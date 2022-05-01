1bc1a813dd791361c2760c0de002a8339ac39fb7
Anton Kasheuski <kascheikinen@gmail.com>
2022-05-01 21:03:32 +0300

    State, functions to change state

--- src/features/CreateNewPassword.tsx
+++ src/features/CreateNewPassword.tsx
@@ -1,8 +1,81 @@
 import s from './CreateNewPassword.module.css';
 import SuperButton from "../CommonComponents/c2-SuperButton/SuperButton";
-
+import {ChangeEvent, useState} from "react";
+import {RegistrationAC} from "../store/redusers/createPassword-reducer";
+import {useDispatch} from "react-redux";
 
 export const CreateNewPassword = () => {
+    const dispatch = useDispatch();
+
+    const [emailInsert, setEmailInsert] = useState<string>('')
+    const [passwordInsert, setPasswordInsertInsert] = useState<string>('')
+    const [confirmPasswordInsert, setConfirmPasswordInsertInsert] = useState<string>('')
+    const [emailError, setEmailError] = useState<string>('')
+    const [passwordError, setPasswordInsertError] = useState<string>('')
+    const [confirmPasswordError, setConfirmPasswordInsertError] = useState<string>('')
+
+    const emailInsertOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmailInsert(e.currentTarget.value)
+    }
+    const passwordInsertOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setPasswordInsertInsert(e.currentTarget.value)
+    }
+    const confirmPasswordInsertOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setConfirmPasswordInsertInsert(e.currentTarget.value)
+    }
+    const emailInsertOnFocusHandler = () => {
+        setEmailError('')
+    }
+    const passwordInsertOnFocusHandler = () => {
+        setPasswordInsertError('')
+    }
+    const confirmPasswordInsertOnFocusHandler = () => {
+        setConfirmPasswordInsertError('')
+    }
+    const clearFormHandler = () => {
+        setEmailInsert('')
+        setPasswordInsertInsert('')
+        setConfirmPasswordInsertInsert('')
+    }
+    const registrationHandler = () => {
+        let error = false;
+        if (emailInsert) {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(emailInsert)) {
+                setEmailError('Invalid email address')
+                error = true
+            }
+        } else {
+            setEmailError('Email required')
+            error = true
+        }
+        if (passwordInsert !== confirmPasswordInsert) {
+            setConfirmPasswordInsertError('Check password')
+            error = true
+        }
+        if (passwordInsert) {
+            if (passwordInsert.length < 4) {
+                setPasswordInsertError('Should be 4 or more symbols')
+                error = true
+            }
+        } else {
+            setPasswordInsertError('Password required')
+            error = true
+        }
+        if (confirmPasswordInsert) {
+            if (confirmPasswordInsert.length < 4) {
+                setConfirmPasswordInsertError('Should be 4 or more symbols')
+                error = true
+            }
+        } else {
+            setConfirmPasswordInsertError('Confirm password required')
+            error = true
+        }
+        if (!error) {
+            dispatch(RegistrationAC(emailInsert, passwordInsert))
+            clearFormHandler()
+        }
+    }
+
     return (
         <div className={s.main}>
             <div className={s.form}>
@@ -12,30 +85,47 @@ export const CreateNewPassword = () => {
                 </div>
                 <div className={s.inputsBlock}>
                     <div>
-                        <div className={s.inputDescription}>Email</div>
+                        <span className={s.inputDescription}>Email   </span>
+                        <span className={s.error}>{emailError}</span>
                         <input
                             className={s.input}
                             type={"email"}
+                            value={emailInsert}
+                            onChange={emailInsertOnChangeHandler}
+                            onFocus={emailInsertOnFocusHandler}
                         />
                     </div>
                     <div>
-                        <div className={s.inputDescription}>Password</div>
+                        <span className={s.inputDescription}>Password   </span>
+                        <span className={s.error}>{passwordError}</span>
                         <input
                             className={s.input}
                             type={"password"}
+                            value={passwordInsert}
+                            onChange={passwordInsertOnChangeHandler}
+                            onFocus={passwordInsertOnFocusHandler}
                         />
                     </div>
                     <div>
-                        <div className={s.inputDescription}>Confirm password</div>
+                        <span className={s.inputDescription}>Confirm password   </span>
+                        <span className={s.error}>{confirmPasswordError}</span>
                         <input
                             className={s.input}
                             type={"password"}
+                            value={confirmPasswordInsert}
+                            onChange={confirmPasswordInsertOnChangeHandler}
+                            onFocus={confirmPasswordInsertOnFocusHandler}
                         />
                     </div>
                 </div>
                 <div className={s.buttonBlock}>
-                    <SuperButton>Cancel</SuperButton>
-                    <SuperButton className={s.blue}>Register</SuperButton>
+                    <SuperButton
+                        onClick={clearFormHandler}
+                    >Cancel</SuperButton>
+                    <SuperButton
+                        className={s.blue}
+                        onClick={registrationHandler}
+                    >Register</SuperButton>
                 </div>
             </div>
         </div>
--- src/store/redusers/createPassword-reducer.tsx
+++ src/store/redusers/createPassword-reducer.tsx
@@ -1,10 +1,27 @@
 
+export type CreatePasswordInitialStateType = {
+    email: string
+    password: string
+}
 
+const createPasswordInitialState: CreatePasswordInitialStateType = {
+    email: '',
+    password: '',
+}
 
-export const createPasswordReducer =(state: any ={}, action: any): any => {
-    switch (action) {
-        case '' :
+export const createPasswordReducer = (state: CreatePasswordInitialStateType = createPasswordInitialState, action: CreatePasswordActionType): CreatePasswordInitialStateType => {
+    switch (action.type) {
+        case 'REGISTRATION':
+            return {...state, email: action.email, password: action.password}
+        default:
             return state
-        default: return state
     }
 }
+
+export const RegistrationAC = (email: string, password: string) => {
+    return {type: 'REGISTRATION', email, password} as const
+}
+
+export type RegistrationAT = ReturnType<typeof RegistrationAC>
+
+export type CreatePasswordActionType = RegistrationAT
