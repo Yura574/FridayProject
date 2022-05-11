be441b5d4d7ce58b345858c82b1748240913fe2c
Timmy2324 <salikhov.t.s@yandex.ru>
2022-05-11 22:47:35 +0400

    added empty cards reducer

---
+++ src/store/redusers/cards-reducer.ts
@@ -0,0 +1,21 @@
+
+enum CardsAction {
+
+}
+
+type InitialState = {
+
+}
+
+const initialState: InitialState = {
+
+}
+
+type CardsActionTypes = any;
+
+export const cardsReducer = (state: InitialState = initialState, action: CardsActionTypes) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
