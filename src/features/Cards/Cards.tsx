27b1ea1643a12a7a1593b65d94b9f14fb3bf7551
Timmy2324 <salikhov.t.s@yandex.ru>
2022-05-13 13:32:49 +0300

    hidden inputs

--- src/features/Cards/Cards.tsx
+++ src/features/Cards/Cards.tsx
@@ -2,15 +2,15 @@ import React, {useEffect, useState} from 'react';
 import s from './Cards.module.css';
 import {useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../../store/store";
+import {AppDispatch, AppRootStateType} from "../../store/store";
 import {addCard, CardType, deleteCard, getCards, updateCard} from "../../store/redusers/cards-reducer";
 
 export const Cards = () => {
     const {cardsPack_id} = useParams();
-    const [question, setQuestion] = useState('');
-    const [answer, setAnswer] = useState('');
+    // const [question, setQuestion] = useState('');
+    // const [answer, setAnswer] = useState('');
 
-    const dispatch = useDispatch<any>();
+    const dispatch = useDispatch<AppDispatch>();
 
     const cards = useSelector<AppRootStateType, CardType[]>(state => state.cards.cards);
 
@@ -63,22 +63,22 @@ export const Cards = () => {
                         Add Card
                     </button>
                 </div>
-                <div>
-                    <input
-                        className={s.searchInput}
-                        placeholder={'Search question...'}
-                        value={question}
-                        onChange={(e) => setQuestion(e.currentTarget.value)}
-                    />
-                </div>
-                <div>
-                    <input
-                        className={s.searchInput}
-                        placeholder={'Search answer...'}
-                        value={answer}
-                        onChange={(e) => setAnswer(e.currentTarget.value)}
-                    />
-                </div>
+                {/*<div>*/}
+                {/*    <input*/}
+                {/*        className={s.searchInput}*/}
+                {/*        placeholder={'Search question...'}*/}
+                {/*        value={question}*/}
+                {/*        onChange={(e) => setQuestion(e.currentTarget.value)}*/}
+                {/*    />*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                {/*    <input*/}
+                {/*        className={s.searchInput}*/}
+                {/*        placeholder={'Search answer...'}*/}
+                {/*        value={answer}*/}
+                {/*        onChange={(e) => setAnswer(e.currentTarget.value)}*/}
+                {/*    />*/}
+                {/*</div>*/}
                 <table className={s.table}>
                     <thead className={s.tableHead}>
                         <tr className={s.tableTitleRow}><th>Question</th><th>Answer</th><th>Last Updated</th><th>Grade</th><th></th></tr>
